5ac5cb8ebad731bffbc51eaad9b9dd8468e27733
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-12-15 11:41:23 +0100

    Add Home, footer and header html and css

--- client/README.md
+++ client/README.md
@@ -1,6 +1,7 @@
-
+# The Bear minimum project
 
 ## Initial setup
 - [x] Initialize react + vite project 
 - [x] Create root level client and server folders
 - [x] Copy softuni-practice-server into server directory
+- [x] Add Home, footer and header html and css
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,52 @@
+export default function Home() {
+    return (
+        <>
+            {/* <!-- Header --> */}
+            < header >
+                <nav>
+                    <h1 class="logo">The Bear Minimum </h1>
+                    <ul class="nav-links">
+                        <li><a href="#">Bears</a></li>
+                        <li><a href="#">My Bears</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                    </ul>
+                </nav>
+            </header >
+
+            {/* <!-- Main Content --> */}
+            < main >
+                <h4>When Time is not a luxury you can afford</h4>
+                <h1>Latest Bears</h1>
+
+                <section class="cards">
+                    <article class="card">
+                        <img src="https://via.placeholder.com/300x200" alt="Fluffy Bear" />
+                        <h3>Fluffy Bear</h3>
+                        <p>Soft and cuddly teddy bear.</p>
+                        <button>Details</button>
+                    </article>
+
+                    <article class="card">
+                        <img src="https://via.placeholder.com/300x200" alt="Brown Bear" />
+                        <h3>Brown Bear</h3>
+                        <p>Classic brown teddy bear.</p>
+                        <button>Details</button>
+                    </article>
+
+                    <article class="card">
+                        <img src="https://via.placeholder.com/300x200" alt="Panda Bear" />
+                        <h3>Panda Bear</h3>
+                        <p>Adorable panda teddy bear </p>
+                        <button>Details</button>
+                    </article>
+                </section>
+            </main >
+            {/* <!-- Footer --> */}
+            < footer >
+                <p>&copy; This site is a bear minimum project course for ReactJS in SoftUni-Bulgaria.</p>
+            </footer >
+        </>
+    )
+}
