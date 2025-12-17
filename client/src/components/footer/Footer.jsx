d54c531e76f5babafc8e3998b10a745dbc31d48f
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-12-17 11:39:51 +0100

    Edit Footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return (
+        <>
+        <p>I cannot even fix the footer</p>
         < footer >
             <p>&copy; This site is a bear minimum project course for ReactJS in SoftUni-Bulgaria.</p>
         </footer >
+    </>
     )
 }
